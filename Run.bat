de11047860badc1f48458b336062247ba1d9fc17
Blushney <82585450+Blushney@users.noreply.github.com>
2024-06-14 13:36:21 +0200

    Add files via upload

---
+++ Run.bat
@@ -0,0 +1,88 @@
+@echo off
+
+:main_menu
+cls
+echo Main Menu
+echo.
+echo Options select
+echo.
+echo [1] Configure Gamepasses
+echo [2] Setup Gamepasses
+echo [3] Robux Amount
+echo.
+set /p option=
+if %option% == 1 goto configure_gamepass
+if %option% == 2 goto setup_gamepass
+if %option% == 3 goto robux_amount
+pause
+goto main_menu
+
+:configure_gamepass
+cls
+echo Configure Gamepasses
+echo.
+
+set /p UniverseId=Enter Universe ID: 
+set /p GamepassesPrice=Enter Prices (comma-separated): 
+
+echo Running configureGamepasses.js with UniverseId=%UniverseId% and GamepassesPrice=%GamepassesPrice%...
+
+rem Run the Node.js script and capture its output
+node ./utilities/configureGamepasses.js %UniverseId% %GamepassesPrice%
+set ExitCode=%errorlevel%
+
+if %ExitCode% == 0 (
+    echo Configure Gamepasses completed successfully.
+) else (
+    echo Configure Gamepasses failed. Error code: %ExitCode%
+)
+pause
+goto main_menu
+
+:setup_gamepass
+cls
+echo Setup Gamepasses
+echo.
+
+set /p PlaceId=Enter PlaceId:
+set /p NumberOfGamePasses=Enter Number of Game Passes:
+echo.
+echo enter the image link (leave blank to use Donation.png)
+echo.
+set /p imageLink=
+
+echo Running gamepass.js
+
+rem Run the Node.js script and capture its output
+node ./utilities/gamepass.js %PlaceId% %NumberOfGamePasses% "%imageLink%"
+set ExitCode=%errorlevel%
+
+if %ExitCode% == 0 (
+    echo Setup Gamepasses completed successfully.
+) else (
+    echo Setup Gamepasses failed. Error code: %ExitCode%
+)
+pause
+goto main_menu
+
+:robux_amount
+cls
+echo Robux Amount
+echo.
+
+echo Running robuxAmount.js
+
+rem Run the Node.js script and capture its output
+node ./utilities/robuxAmount.js
+set ExitCode=%errorlevel%
+
+if %ExitCode% == 0 (
+    echo Robux Amount completed successfully.
+) else (
+    echo Robux Amount failed. Error code: %ExitCode%
+)
+pause
+goto main_menu
+
+:end
+pause
